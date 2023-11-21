43e27f24472d03ac9028931ad9836cf2a12e3212
FlorA <upto0013@gmail.com>
2023-11-20 20:34:20 -0600

    Update SM_test.py

--- SM_test.py
+++ SM_test.py
@@ -12,12 +12,17 @@ max_moisture=49300
 readDelay = 0.5 # delay between readings
 
 
+#setup LED
+led = machine.Pin("LED", machine.Pin.OUT)
+
 while True:
+led.on()
 # read moisture value and convert to percentage into the calibration range
 moisture = (max_moisture-soil.read_u16())*100/(max_moisture-min_moisture) 
 
 # print values
 print("moisture: " + "%.2f" % moisture +"% (adc: "+str(soil.read_u16())+")")
 
-# set a delay between readings 
+# set a delay between readings
+led.off()
 utime.sleep(readDelay)    
